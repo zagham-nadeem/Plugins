cfb6019a45f552ca510d4815ad69b00d1b2832be
Zagham Nadeem <Zagham@learn2earnn.com>
2022-10-26 12:06:44 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zagham.plugins',
+  appName: 'Plugins',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
